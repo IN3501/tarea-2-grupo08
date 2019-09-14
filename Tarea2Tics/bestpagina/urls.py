8ea44d2043ae64c168e8a04939234c4d57313f1d
Ismael Orrego <ismael.orrego@ing.uchile.cl>
2019-09-14 02:32:16 -0300

    Formularios hechos

--- Tarea2Tics/bestpagina/urls.py
+++ Tarea2Tics/bestpagina/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', blank, name='blank'),
-    path('checkout/',checkout,name='checkout')
+    path('checkout/',checkout,name='checkout'),
+    path('signup/',signup,name='signup'),
+    path('contact/',contact,name='contact'),
+    path('enviado/',enviado,name='enviado'),
 ]
--- Tarea2Tics/bestpagina/views.py
+++ Tarea2Tics/bestpagina/views.py
@@ -6,3 +6,13 @@ def blank(request):
 
 def checkout(request):
     return render(request,'checkout.html')
+
+def signup(request):
+	return render(request,'signup.html')
+
+def contact(request):
+	return render(request,'contact.html')
+
+def enviado(request):
+	return render(request,'enviado.html')
+
